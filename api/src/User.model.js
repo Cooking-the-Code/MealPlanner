7602b5c8b66887db5dea1f3da8712c979903a671
Liang Liang <lilysway@hotmail.com>
2020-08-19 17:34:11 -0400

    building onto user schema

--- api/src/User.model.js
+++ api/src/User.model.js
@@ -2,8 +2,26 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   username: {
-    type: String
-  }
+    type: String,
+    require: true,
+    unique: true,
+  },
+  firstName: {
+    type: String,
+    require: true,
+  },
+  lastName: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
 });
 
 const User = mongoose.model("User", userSchema);

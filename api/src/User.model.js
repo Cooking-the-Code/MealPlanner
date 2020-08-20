2065d1820494db6fd8453f9d4311d073a71a2b8f
Liang Liang <lilysway@hotmail.com>
2020-08-20 16:43:11 -0400

    hasing password algo, passport local and jwt added

--- api/src/User.model.js
+++ api/src/User.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -22,7 +23,36 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
   },
+  recipes: [{ type: mongoose.Schema.Types.ObjectId, ref: "recipe" }],
 });
+
+//pre save hook for hashing the password before saving it to the database
+userSchema.pre("save", function(next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  bcrypt.hash(this.password, 10, (err, passwordHashed) => {
+    if (err) {
+      return next(err);
+    }
+    this.password = passwordHashed;
+    next();
+  });
+});
+
+//validating the password entered by user to the password stored in db
+userSchema.methods.comparePassword = function(password, cb) {
+  bcrypt.compare(password, this.password, (err, isMatch) => {
+    if (err) {
+      return cb(err);
+    } else {
+      if (!isMatch) {
+        return cb(null, isMatch);
+      }
+      return null, this; //this is the user
+    }
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 

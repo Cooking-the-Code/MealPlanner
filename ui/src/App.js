4d55c87260e36fdb07ca00965e61fdc359c5ae08
jyoti <jshindems@gmail.com>
2020-09-09 10:17:32 -0400

    react-auth

--- ui/src/App.js
+++ ui/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './assets/greenMealPlanIcon.svg';
 import axios from 'axios';
 import './css/App.scss';
+import Login from './components/Login';
 
 const apiUrl = `http://localhost:8080`;
 
@@ -39,6 +40,7 @@ class App extends Component {
             ))}
           </ul>
         </header>
+        <Login />
       </div>
     );
   }

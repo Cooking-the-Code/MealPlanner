762e232a356d4309902164b3bfb1e2f830133782
Beth-A <elizabeth78@me.com>
2020-09-23 14:46:04 -0400

    add context files to Langing.js

--- ui/src/pages/Landing.js
+++ ui/src/pages/Landing.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import CTA from '../contexts/LandingCTA';
+import Hero from '../contexts/LandingHero';
+import How from '../contexts/LandingHow';
+import Testimonial from '../contexts/LandingTestimonials';
+import LandingFooter from  '../contexts/LandingFooter';
+
 
 class Landing extends Component {
 
@@ -6,7 +12,14 @@ class Landing extends Component {
 
 render() {
 	return (
-		
+        <div>
+            <Hero />
+            <How />
+            <Testimonial />
+            <CTA />
+            <LandingFooter />
+        </div>
+
 		);
 	}
 }

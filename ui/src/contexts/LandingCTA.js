db411341d12dd77adb6432e91cf55ce8a8b237a3
Beth-A <elizabeth78@me.com>
2020-09-26 22:43:56 -0400

    added CTAButton reusable component into LandingCTA.js

--- ui/src/contexts/LandingCTA.js
+++ ui/src/contexts/LandingCTA.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, Container, Typography } from '@material-ui/core';
+import CTAButton from '../components/CTAButton';
 
 export default function CTA() {
     // path for sign up page
@@ -9,8 +10,12 @@ export default function CTA() {
     return (
         <div display="flex">
             <Container maxWidth={false} style={{margin:"0", padding: "0"}}>
-                <Typography component="div" style={{backgroundColor: '#127681', height: 'auto', margin: 'auto', border:'5px solid yellow'}}>
-                    <Typography variant="h3"  style={{
+                <Typography 
+                    component="div" 
+                    style={{backgroundColor: '#127681', height: 'auto', margin: 'auto', border:'5px solid yellow'}}>
+                    <Typography 
+                        variant="h3"  
+                        style={{
                             textAlign: 'center', margin: '10vh', 
                             marginLeft: '20vh', marginRight: '20vh',
                             border:'5px solid red'}}>
@@ -19,13 +24,16 @@ export default function CTA() {
                         Chitty Chitty Bang Bang, Chitty Chitty Bang Bang
                         What we'll do?
                     </Typography>
-                    <Typography variant="h4" align="center" style={{
+                    <Typography 
+                        variant="h4" 
+                        align="center" 
+                        style={{
                             textAlign: 'center', margin: '10vh', 
-                            marginLeft: '20vh', marginRight: '20vh',
+                            marginLeft: '20vh', marginRight: '20vh', 
                             border:'5px solid red'}}>
                         Click on the CTA!
                         <br />
-                        <Button color='primary' variant='contained' style={{background: '#fac70b', color: '#21174a', fontSize: '1.6rem'}}>Join Now</Button>
+                        <CTAButton />
                     </Typography>
                 </Typography>   
             </Container>

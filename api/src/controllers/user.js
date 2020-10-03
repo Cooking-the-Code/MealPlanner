28599d6da8f838d3f10f3e6ba5f77d7d1ebbefd7
melvin-viana <mjviana057@gmail.com>
2020-10-02 22:45:45 -0700

    Add put route for email validation

--- api/src/controllers/user.js
+++ api/src/controllers/user.js
@@ -4,6 +4,8 @@ const bcrypt = require('bcrypt');
 const { createUserToken } = require('../middleware/auth');
 const { requireToken } = require('../middleware/auth');
 const { sendEmail } = require('../middleware/validateEmail');
+const { validateEmail } = require('../middleware/validateEmail');
+
 const User = require('../User.model.js');
 
 router.post('/signup', async (req, res, next) => {
@@ -11,14 +13,15 @@ router.post('/signup', async (req, res, next) => {
 		// console.log('req.body:', req.body);
 		const password = await bcrypt.hash(req.body.password, 10);
 		const user = await User.create({
-			username:req.body.username,
+			username:req.body.username, // Is username necessary?
 			firstName: req.body.firstName,
 			lastName: req.body.lastName,
 			email: req.body.email,
 			password,
 		});
+		//Sends an email
 		sendEmail(user.email, user.username);
-		// res.status(201).json(user);
+		res.status(201).json(user);
 	} catch (error) {
 		return next(error);
 	}
@@ -31,6 +34,20 @@ router.post('/signin', (req, res, next) => {
 		.catch(next);
 });
 
+// Validates email
+router.put('/validate/:email', (req,res, next) => {
+	validateEmail(req.params.email)
+		.then(results=> {
+			//If email doesn't exist in database send 404
+			if (!results) res.sendStatus(404);
+			//otherwise  send 200 status, data should be updated
+			res.sendStatus(200);
+		})
+		.catch(err=> {
+			return next(err);
+			res.sendStatus(404);
+		})
+});
 
 
 module.exports = router;

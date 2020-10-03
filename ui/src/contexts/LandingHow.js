1514964a3cfd34106c321a01eca3a1cffe505d88
Beth-A <elizabeth78@me.com>
2020-10-03 14:27:05 -0400

    worked on landing page responsiveness & added some new images

--- ui/src/contexts/LandingHow.js
+++ ui/src/contexts/LandingHow.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { Container, Grid } from '@material-ui/core';
 import HowCard from '../components/HowCard';
 import { H4Text }from '../components/Headers';
+import coupleCooking2 from '../assets/coupleCooking2.jpg';
+import planning2 from '../assets/planning2.jpg';
+import flourFamily from '../assets/flourFamily.jpg';
 
 export default function How() {
   const howText1 = "Chitty Bang Bang, Chitty Chitty Bang Bang We love you and in Chitty Chitty Bang Bang, Chitty Chitty Bang Bang What we'll do?";
   const howText2 = "Enough with the silly old school movie referances will ya?";
   const howText3 = "Very well, but the hills are alive!!   Alive with the sound of Music!";  
+  
   
   return (
       <div>
@@ -17,9 +21,9 @@ export default function How() {
             // border:'5px solid black'
           }}>
             
-            <HowCard side={'left'} text={howText1} image={'placeHolder'}/>
-            <HowCard side={'right'} text={howText2} image={'placeHolder'}/>
-            <HowCard side={'left'} text={howText3} image={'placeHolder'}/>
+            <HowCard side={'left'} text={howText1} image={'placeholder'}/>
+            <HowCard side={'right'} text={howText2} image={'placeholder'}/>
+            <HowCard side={'left'} text={howText3} image={'placeholder'}/>
           </Grid>
         </Container> 
       </div>    

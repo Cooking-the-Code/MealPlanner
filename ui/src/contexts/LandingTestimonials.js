a7289a14d0d037126e7729ba2ce4a5bd6ca67799
Beth-A <elizabeth78@me.com>
2020-10-06 19:58:07 -0400

    worked on testimonials

--- ui/src/contexts/LandingTestimonials.js
+++ ui/src/contexts/LandingTestimonials.js
@@ -1,26 +1,19 @@
 import React from 'react';
 import { Container, Typography } from '@material-ui/core';
 import { H5Text } from '../components/Headers';
-import { AutoRotatingCarousel, Slide } from 'material-auto-rotating-carousel';
 
 export default function Testimonial() {
   // const { red, blue, green } = require('@material-ui/core/colors');
-
+  
   return (
     <div style={{ position: 'relative', width: '100%'}}>
-      <Container maxWidth={false} style={{margin:"0", padding: "0", border:'5px solid yellowgreen'}}>
-        <Typography component="div" style={{height: '75vh'}}>
-          <H5Text/>
-          <AutoRotatingCarousel style={{ border: '5px solid purple' }}>
-            <Slide
-              media={<img src='../assets/coupleCooking.jpg'/>}
-              title='User Review of Come to the Table App'
-              subtitle='User Name'
-            />
-          </AutoRotatingCarousel>
+      <Container maxWidth={false} style={{margin:'0', padding: '0', border:'5px solid yellowgreen'}}>
+        <Typography component='div' style={{height: '75vh', border: '5px dotted blue'}}>
+          <H5Text />
+          
         </Typography>
       </Container> 
     </div>
+      
   );
 }
-    

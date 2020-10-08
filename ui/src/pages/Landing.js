99987c43d746f81246b6d72803cca5532206fe70
Mark Willisford <MarkWillisford@users.noreply.github.com>
2020-10-07 18:23:37 -0700

    update landing

--- ui/src/pages/Landing.js
+++ ui/src/pages/Landing.js
@@ -4,6 +4,7 @@ import Hero from '../contexts/LandingHero';
 import How from '../contexts/LandingHow';
 import Testimonial from '../contexts/LandingTestimonials';
 import LandingFooter from  '../contexts/LandingFooter';
+import { Grid } from '@material-ui/core';
 
 
 class Landing extends Component {
